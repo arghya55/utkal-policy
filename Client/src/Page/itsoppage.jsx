fd6fa93efa645d222a2f6c8794cd3d28926968f7
Arghya <arghyadey043@gmail.com>
2026-04-20 15:23:39 +0530

    Updated some features / fixed bugs

--- Client/src/Page/itsoppage.jsx
+++ Client/src/Page/itsoppage.jsx
@@ -7,7 +7,7 @@ const ItSopPage = () => {
       <h1 className='itsop-header'>IT SOP</h1>
       <p className='itsop-text'>This is the IT SOP page.</p>
 
-      <a href="#/department/it" className="back-link">
+      <a href="#/department/it" className="back2-link">
         Back to IT Department
       </a>
 
