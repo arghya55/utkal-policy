0d3044ff29ca26e32c6f5680765e3b1a1100f993
Arghya <arghyadey043@gmail.com>
2026-04-20 15:04:55 +0530

    Updated video component

--- Client/src/Video/Video.jsx
+++ Client/src/Video/Video.jsx
@@ -1,65 +1,63 @@
 import React, { useState } from 'react'
 import './Video.css'
-import EMRVideo from '../videos/EMR.mp4'
 
 const Video = () => {
   const videos = [
     {
       id: 1,
       title: 'EMR',
-      src: EMRVideo
+      src: '' // add Cloudinary later
     },
     {
       id: 2,
-      title: 'IT Training Video 2',
-      src: ''
+      title: 'INVENTORY - DEPT. CONSUMPTION PROCESS',
+      src: 'https://res.cloudinary.com/dy5eigldg/video/upload/v1776675504/INVENTORY_-_DEPT._CONSUMPTION_PROCESS_mwvffi.mp4'
     },
     {
       id: 3,
-      title: 'IT Training Video 3',
-      src: ''
-    },
-    // Add more with here video files
+      title: 'WARD - HOW TO SEND DISCHARGE MEDICATION FROM WARD',
+      src: 'https://res.cloudinary.com/dy5eigldg/video/upload/v1776675557/WARD_-_HOW_TO_SEND_DISCHARGE_MEDICATION_FROM_WARD_gor6oz.mp4'
+    }
   ]
 
   const [selectedVideo, setSelectedVideo] = useState(null)
 
-  const handleVideoClick = (video) => {
-    setSelectedVideo(video)
-  }
-
-  const handleBackClick = () => {
-    setSelectedVideo(null)
-  }
-
   return (
     <div className="video-container">
-      <h1 className="video-header">IT Training Videos <br /><br />{selectedVideo ? selectedVideo.title : ''}</h1>
+      <h1 className="video-header">
+        IT Training Videos <br /><br />
+        {selectedVideo ? selectedVideo.title : ''}
+      </h1>
 
       {!selectedVideo ? (
         <div className="video-list">
           {videos.map((video) => (
-            <div key={video.id} className="video-item">
-              <button className="video-button" onClick={() => handleVideoClick(video)}>
-                {video.title}
-              </button>
-            </div>
+            <button
+              key={video.id}
+              className="video-button"
+              onClick={() => setSelectedVideo(video)}
+            >
+              {video.title}
+            </button>
           ))}
         </div>
       ) : (
         <div className="video-player">
-          {/* <h2 className="video-title">{selectedVideo.title}</h2> */}
-          <video width="100%" controls className="video-iframe">
-            <source src={selectedVideo.src} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <button className="back-button" onClick={handleBackClick}>
+          {selectedVideo.src ? (
+            <video width="100%" controls className="video-iframe">
+              <source src={selectedVideo.src} type="video/mp4" />
+            </video>
+          ) : (
+            <p>No video available</p>
+          )}
+
+          <button onClick={() => setSelectedVideo(null)} className='back-link'>
             Back to Video List
           </button>
         </div>
       )}
 
-      <a href="#/department/it" className="back-link">
+      <a href="#/department/it" className="back-button">
         Back to IT Department
       </a>
     </div>
