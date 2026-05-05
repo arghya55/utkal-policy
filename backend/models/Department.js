335f418e6e76f53262452f0fb7576132af7da745
Arghya dey <159042843+arghya55@users.noreply.github.com>
2026-05-05 14:25:05 +0530

    Delete backend directory

--- backend/models/Department.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const departmentSchema = new mongoose.Schema({
-  name: String,
-  code: String
-});
-
-module.exports = mongoose.model("Department", departmentSchema);
